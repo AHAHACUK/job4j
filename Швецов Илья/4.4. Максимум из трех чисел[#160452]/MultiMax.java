5c9d6e3e0fcab374caad65c159c2655601bb6f4f
AHAHACUK <mrtiwesyes@gmail.com>
2019-07-16 21:45:55 +0500

    4.4. Максимум из трех чисел[#160452]

---
+++ Швецов Илья/4.4. Максимум из трех чисел[#160452]/MultiMax.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+    public int max(int first, int second, int third) {
+        int result = first;
+        if (second > result) result = second;
+        else if (third > result) result = third;
+        return result;
+    }
+}
