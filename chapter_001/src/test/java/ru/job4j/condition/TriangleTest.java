9f64f9d0dd5ab12ece66f8b6ef63ac1e99478692
AHAHACUK <mrtiwesyes@gmail.com>
2019-07-17 13:20:18 +0500

    fix

--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 import org.junit.Test;
 
 import static org.hamcrest.number.IsCloseTo.closeTo;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 
 public class TriangleTest {
