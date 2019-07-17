3d0c093059b4ed8b29dc1a9f701ca9a31382a73a
AHAHACUK <mrtiwesyes@gmail.com>
2019-07-17 17:32:10 +0500

    Разделение тестов

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -10,11 +10,19 @@ public class MaxTest {
         Max max = new Max();
         int result = max.max(1, 2);
         assertThat(result, is(2));
+    }
 
-        result = max.max(2, 2);
+    @Test
+    public void whenMax2To2Then2() {
+        Max max = new Max();
+        int result = max.max(2, 2);
         assertThat(result, is(2));
+    }
 
-        result = max.max(2, 1);
+    @Test
+    public void whenMax2To1Then2() {
+        Max max = new Max();
+        int result = max.max(2, 1);
         assertThat(result, is(2));
     }
 }
