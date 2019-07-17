0a6e31d04b9bcacf75269c0b8fe2ae180ccc22ba
AHAHACUK <mrtiwesyes@gmail.com>
2019-07-17 09:34:06 +0500

    clear

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -16,12 +16,4 @@ public class Calculate {
         System.out.println("Hello World");
     }
 
-    /**
-     * Method echo.
-     * @param name Your name.
-     * @return Echo plus your name.
-     */
-    public String echo(String name) {
-        return "Echo, echo, echo : " + name;
-    }
 }
