6a4893987ec6c4ca3627019f06881574319ebd0f
AHAHACUK <mrtiwesyes@gmail.com>
2019-07-17 13:36:49 +0500

    PrimeNumber

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public int calc(int finish) {
+        int count = 0;
+        for (int numToCheck = finish;numToCheck>1;numToCheck--) {
+            boolean isPrime = true;
+            for (int divider = 2; divider<numToCheck; divider++) {
+                if (numToCheck%divider==0) isPrime=false;
+            }
+            if (isPrime) count++;
+        }
+        return count;
+    }
+}
