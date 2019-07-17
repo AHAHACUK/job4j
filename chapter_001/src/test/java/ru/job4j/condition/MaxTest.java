19ba9b9b401211a6b0c4ae0626d978e6a635362f
AHAHACUK <mrtiwesyes@gmail.com>
2019-07-17 11:18:03 +0500

    Новые тесты

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -10,5 +10,11 @@ public class MaxTest {
         Max max = new Max();
         int result = max.max(1, 2);
         assertThat(result, is(2));
+
+        result = max.max(2, 2);
+        assertThat(result, is(2));
+
+        result = max.max(2, 1);
+        assertThat(result, is(2));
     }
 }
