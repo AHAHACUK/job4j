539f0f2a98458b956d464f28edbeb937c1993f43
AHAHACUK <mrtiwesyes@gmail.com>
2019-07-18 16:41:46 +0500

    MatrixCheck

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class MatrixCheck {
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        int len = data.length;
+        for (int i = 0; i < data.length-1; i++) {
+            if (data[i][i]!=data[i+1][i+1] || data[i][len-i-1]!=data[i+1][len-i-2]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
