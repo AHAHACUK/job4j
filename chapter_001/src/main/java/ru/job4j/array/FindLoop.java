409380b963c106162ce65ab2c319881fa7f74e59
AHAHACUK <mrtiwesyes@gmail.com>
2019-07-22 12:44:06 +0500

    StyleUpdate

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,8 +1,14 @@
 package ru.job4j.array;
 
 public class FindLoop {
+    /**
+     * Метод определяет и возвращает индекс элемента в массиве
+     * @param data Массив, в котором находится искомый элемент
+     * @param el Элемент, индекс которого нужен
+     * @return int индекс элемента
+     */
     public int indexOf(int[] data, int el) {
-        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        int rst = -1;
         for (int index = 0; index < data.length; index++) {
             if (data[index] == el) {
                 rst = index;
@@ -12,6 +18,14 @@ public class FindLoop {
         return rst;
     }
 
+    /**
+     * Метод определяет и возвращает индекс элемента в определённом промежутке массива
+     * @param data Массив, в котором находится искомый элемент
+     * @param el Элемент, индекс которого нужен
+     * @param start Индекс, с которого начинается поиск. Т.е. левая граница
+     * @param finish Индекс, которым заканчивается поиск. Т.е правая граница
+     * @return int индекс элемента
+     */
     public int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1;
         for (int i = start; i <= finish; i++) {
@@ -23,6 +37,11 @@ public class FindLoop {
         return rst;
     }
 
+    /**
+     * Метод сортирует массив в порядке возростания
+     * @param data Массив, подлежащий сортировке
+     * @return int[] отсортированый массив
+     */
     public int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = data[i];
