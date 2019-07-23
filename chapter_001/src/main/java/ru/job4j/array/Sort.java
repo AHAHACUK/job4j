f94aa7f56202449e81aa43938a7c870a47105680
AHAHACUK <mrtiwesyes@gmail.com>
2019-07-23 15:49:16 +0500

    style

--- chapter_001/src/main/java/ru/job4j/array/Sort.java
+++ chapter_001/src/main/java/ru/job4j/array/Sort.java
@@ -2,10 +2,10 @@ package ru.job4j.array;
 
 public class Sort {
 
-    public int[] merge(int[] A, int[] B) {
-        int[] small = A.length < B.length ? A : B;
-        int[] big = A.length < B.length ? B : A;
-        int[] result = new int[A.length+B.length];
+    public int[] merge(int[] a, int[] b) {
+        int[] small = a.length < b.length ? a : b;
+        int[] big = a.length < b.length ? b : a;
+        int[] result = new int[a.length + b.length];
         for (int i = 0; i < small.length; i++) {
             if (small[i] < big[i]) {
                 result[i * 2] = small[i];
--- chapter_001/src/test/java/ru/job4j/array/SortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SortTest.java
@@ -10,9 +10,9 @@ public class SortTest {
     @Test
     public void when2() {
         Sort sort = new Sort();
-        int[] inA = {1,3};
-        int[] inB = {2,4};
-        int[] expected = {1,2,3,4};
+        int[] inA = {1, 3};
+        int[] inB = {2, 4};
+        int[] expected = {1, 2, 3, 4};
         int[] out = sort.merge(inA, inB);
         assertThat(out, is(expected));
     }
@@ -20,9 +20,9 @@ public class SortTest {
     @Test
     public void when5() {
         Sort sort = new Sort();
-        int[] inA = {1,3,6,10,15};
-        int[] inB = {2,4,7,8,11};
-        int[] expected = {1,2,3,4,6,7,8,10,11,15};
+        int[] inA = {1, 3, 6, 10, 15};
+        int[] inB = {2, 4, 7, 8, 11};
+        int[] expected = {1, 2, 3, 4, 6, 7, 8, 10, 11, 15};
         int[] out = sort.merge(inA, inB);
         assertThat(out, is(expected));
     }
@@ -30,9 +30,9 @@ public class SortTest {
     @Test
     public void when3and5() {
         Sort sort = new Sort();
-        int[] inA = {1,8,9};
-        int[] inB = {2,7,10,11,12};
-        int[] expected = {1,2,7,8,9,10,11,12};
+        int[] inA = {1, 8, 9};
+        int[] inB = {2, 7, 10, 11, 12};
+        int[] expected = {1, 2, 7, 8, 9, 10, 11, 12};
         int[] out = sort.merge(inA, inB);
         assertThat(out, is(expected));
     }
