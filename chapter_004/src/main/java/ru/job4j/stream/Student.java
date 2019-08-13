af044e20b600c373b03379ccac95b0ca56d69e1e
AHAHACUK <mrtiwesyes@gmail.com>
2019-08-13 10:50:48 +0500

    Update Student.java

--- chapter_004/src/main/java/ru/job4j/stream/Student.java
+++ chapter_004/src/main/java/ru/job4j/stream/Student.java
@@ -1,6 +1,11 @@
 package ru.job4j.stream;
 
-public class Student {
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Student{
     private String surname;
     private int score;
 
@@ -15,5 +20,16 @@ public class Student {
 
     public String getSurname() {
         return surname;
+    }
+
+    List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream().sorted(
+                new Comparator<Student>() {
+                    @Override
+                    public int compare(Student o1, Student o2) {
+                        return Integer.compare(o1.getScore(), o2.getScore());
+                    }
+                }
+        ).flatMap(Stream::ofNullable).takeWhile(e -> e.getScore() > bound).collect(Collectors.toList());
     }
 }
